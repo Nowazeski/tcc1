3f629ecd60dbf8042e20a602b7c6a4e73954a723
Ana Karina <karina.weneski@gmail.com>
2022-09-20 20:07:18 -0300

    Criada tela login, correção e extensão navbar;

--- src/rotas.js
+++ src/rotas.js
@@ -4,10 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Entrar from "./pages/Entrar";
 import Cadastrar from "./pages/Cadastrar/index.jsx";
-import NavBar from "./components/NavBar.js";
+import NavBar from "./components/NavBar.jsx";
 import Footer from "./components/Footer";
-import Card1 from "./components/Card1";
-
+import Login1 from "./pages/Login/index.jsx";
 
 function Rotas() {
 
@@ -15,12 +14,12 @@ function Rotas() {
     <BrowserRouter>
       <NavBar></NavBar>
       <Routes>
-        <Route path="/" exact element={<Home />}></Route>
-        <Route path="/pages/Entrar/index.jsx" exact element={<Entrar />}></Route>
-        <Route path="/pages/Cadastrar/index.jsx" exact element={<Cadastrar />}></Route>
+        <Route path="/" exact element={<Login1/>}></Route>
       </Routes>
       <Footer></Footer>
     </BrowserRouter>
+        //<Route path="/pages/Entrar/index.jsx" exact element={<Entrar />}></Route>
+        //<Route path="/pages/Cadastrar/index.jsx" exact element={<Cadastrar />}></Route>
   );
 };
 
