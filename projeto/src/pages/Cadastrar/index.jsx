b4ba69683a35fd9c45639acb7344cd25babed37e
lukax94 <46107707+lukax94@users.noreply.github.com>
2022-10-07 23:00:57 -0300

    tela cadastro dimensionada

--- projeto/src/pages/Cadastrar/index.jsx
+++ projeto/src/pages/Cadastrar/index.jsx
@@ -12,56 +12,61 @@ function Cadastrar() {
   return (
     <>
 
-      <h1>Cadastro do Professor</h1>
+      <bold1>Cadastro do Professor</bold1>
     
-    
-      <Form.Floating className="mb-3">
+      <Col xs={3}>
+      <><Form.Floating className="mb-3">
         <Form.Control
           id="floatingInputCustom"
           type="name"
           placeholder="name"
+          size="lg" 
         />
         <label htmlFor="floatingInputCustom">Nome</label>
-      </Form.Floating>
+      </Form.Floating></></Col>
 
-      <Form.Floating>
+      <Col xs={3}>
+      <><Form.Floating>
         <Form.Control
           id="floatingInputCustom"
           type="email"
           placeholder="name@example.com"
         />
         <label htmlFor="floatingInputCustom">Email</label>
-      </Form.Floating>
+      </Form.Floating></></Col>
 
-      <Form.Floating className="mb-3">
+      <Col xs={3}>
+      <div><Form.Floating className="mb-3">
         <Form.Control
           id="floatingPasswordCustom"
           type="password"
           placeholder="name@example.com"
         />
         <label htmlFor="floatingPasswordCustom">Senha</label>
-      </Form.Floating>
+      </Form.Floating></div></Col>
 
-      <Form.Floating>
+      <Col xs={3}>
+      <div><Form.Floating>
         <Form.Control
           id="floatingPasswordCustom"
           type="password"
           placeholder="Password"
         />
         <label htmlFor="floatingPasswordCustom">Confirme a Senha</label>
-      </Form.Floating>
+      </Form.Floating></div></Col>
 
-      
-      <Form.Floating>
+      <Col xs={3}>
+     <div><Form.Floating>
         <Form.Control
           id="ffloatingInputCustom"
           type="date"
           placeholder="DD/MM/YYYY"
         />
         <label htmlFor="floatingInputCustom">Data de Cadastro</label>
-      </Form.Floating>
+      </Form.Floating></div></Col>
 
-      <Button variant="primary">Enviar</Button>{' '}
+      <div> <Button variant="primary">Enviar</Button>{' '}</div>
+      
     </>
 
 
